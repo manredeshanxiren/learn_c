4422b7160ab2e7dd9da2a10a1cea733011c0a76b
manredeshanxiren <1448554339@qq.com>
2022-08-14 17:15:41 +0800

    测试

--- 喝汽水问题/test.c
+++ 喝汽水问题/test.c
@@ -1,33 +1,53 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-int cacu(int n)
-{
-	int count = 0;
-	int temp = 0;
-	while (n / 2)
-	{
-		if (n % 2 == 1)
-			temp++;
-		count += n / 2;
-		n = n / 2;
-	}
-	if (n == 1)
-		temp++;
-	if (temp / 2 == 0)
-	{
-		return count;
-	}
-	else
-	{
-		return count + cacu(temp);
-	}
-}
+//int cacu(int n)
+//{
+//	int count = 0;
+//	int temp = 0;
+//	while (n / 2)
+//	{
+//		if (n % 2 == 1)
+//			temp++;
+//		count += n / 2;
+//		n = n / 2;
+//	}
+//	if (n == 1)
+//		temp++;
+//	if (temp / 2 == 0)
+//	{
+//		return count;
+//	}
+//	else
+//	{
+//		return count + cacu(temp);
+//	}
+//}
+//int main()
+//{
+//	int n;
+//	int count = 0;
+//	scanf_s("%d", &n);
+//	count += n;
+//	printf("%d", count + cacu(n));
+//	return 0;
+//}
+
+
+//Αν½β
 int main()
 {
-	int n;
-	int count = 0;
-	scanf_s("%d", &n);
-	count += n;
-	printf("%d", count + cacu(n));
+	int money = 0;
+	int total = 0;
+	int empty = 0;
+	scanf("%d", &money);
+	total = money;
+	empty = money;
+	
+	while (empty >= 2)
+	{
+		total += empty / 2;
+		empty = empty / 2 + empty % 2;
+	}
+	printf("%d\n", total);
 	return 0;
 }
