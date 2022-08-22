de4fd060138116ec42e668bec92958712a6318a2
manredeshanxiren <1448554339@qq.com>
2022-08-22 12:16:37 +0800

    更新

--- 不用加减乘除做加法/test.c
+++ 不用加减乘除做加法/test.c
@@ -1,1 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+int Add(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return Add(a ^ b, ((a & b) << 1));
+    }
+};
+
+
