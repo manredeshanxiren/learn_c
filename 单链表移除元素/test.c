a69bd0cd8add1b3f4fee16882239054de83ff5ce
manredeshanxiren <1448554339@qq.com>
2022-09-08 18:56:06 +0800

    力扣刷题

---
+++ 单链表移除元素/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdlib.h>
+//µÝ¹é
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    if (head == NULL) {
+        return head;
+    }
+    head->next = removeElements(head->next, val);
+    return head->val == val ? head->next : head;
+}
+
+//µü´ú
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    if (head == NULL) {
+        return head;
+    }
+    head->next = removeElements(head->next, val);
+    return head->val == val ? head->next : head;
+}
