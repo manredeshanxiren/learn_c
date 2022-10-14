051d4275054d3de7dd2390403a7561c5e77c542f
manredeshanxiren <1448554339@qq.com>
2022-10-14 13:11:21 +0800

    fseek

---
+++ Queue/test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"Queue.h"
+
+void TestQueue1()
+{
+	Queue q;
+	QueueInit(&q);
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+
+	QueuePop(&q);
+	QueuePop(&q);
+	QueuePop(&q);
+	QueuePop(&q);
+
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+
+
+	QueueDestroy(&q);
+}
+
+
+int main()
+{
+	TestQueue1();
+
+	return 0;
+}
