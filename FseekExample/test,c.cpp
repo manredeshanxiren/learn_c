0e2eedaa18c0bd6e0f57c391df4855ebdbd0d3a3
manredeshanxiren <1448554339@qq.com>
2022-10-14 13:11:05 +0800

    fseek

--- FseekExample/test,c.cpp
+++ FseekExample/test,c.cpp
@@ -4,13 +4,15 @@ int main()
 {
     char s [30];
     FILE* pFile;
-    pFile = fopen("example.txt", "wb");
+    pFile = fopen("example.txt", "w+");
     fputs("This is an apple.", pFile);
     fseek(pFile, 9, SEEK_SET);
-    fputs(" sam", pFile);
-    fclose(pFile);
-    pFile = fopen("example.txt", "rb");
+    fputs(" sam", pFile); 
+    rewind(pFile);
     fgets(s, 20,pFile);
     printf("%s", s);
+    fclose(pFile);
+
+
     return 0;
 }
